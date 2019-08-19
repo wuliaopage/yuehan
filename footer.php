d5ba8c23e72ebaac6f18bddb691a79b47240aca4
xufangda <xfangda@gmail.com>
2019-08-19 12:58:26 +0100

    test new layout

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
+</div>  
 </div>
-    <footer id="footer" class="footer">
-        <div class="copyright">
-            <span>© Yuehan Liu </span>
-        </div>
-    </footer>
+<footer id="footer" class="footer">
+    <div class="copyright">
+        <span>© Yuehan Liu </span>
+    </div>
+</footer>
 </div>
 </body>
 </html>
