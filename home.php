60d98275366a233ad6a985b4343b88ef1cd7e17b
xufangda <xfangda@gmail.com>
2019-08-19 12:39:48 +0100

    debug

--- home.php
+++ home.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
 
-<div class="main">
-            <div class="container">
+    <div class="main">
+        <div class="container">
             <div class="intro">
                 <div class="avatar">
                     <a href="/archives"><img src="<?php bloginfo('template_url');?>/image/header.jpg"></a>
                 </div>
                 <div class="nickname">Yuehan Liu</div>
-                <div class="description"><p>PhD Candidate</p>
-            </div>
+                <div class="description"><p>PhD Candidate</p></div>
+            
                 <div class="links">
-                            <a class="link-item" title="Blog" href="/">
-                                <i class="iconfont icon-blog"></i>
-                            </a>
-                        
-                            <a class="link-item" title="ZhiHu" href>
-                                <i class="iconfont icon-zhihu"></i>
-                            </a>
-                        
-                            <a class="link-item" title="Instagram" href>
-                                <i class="iconfont icon-instagram"></i>
-                            </a>
-                </div>
+                    <a class="link-item" title="Blog" href="/">
+                        <i class="iconfont icon-blog"></i>
+                    </a>
+                
+                    <a class="link-item" title="ZhiHu" href>
+                        <i class="iconfont icon-zhihu"></i>
+                    </a>
+                
+                    <a class="link-item" title="Instagram" href>
+                        <i class="iconfont icon-instagram"></i>
+                    </a>
             </div>
         </div>
+    </div>
         
 <?php get_footer(); ?>
