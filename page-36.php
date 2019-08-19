2f379d74585f0e5f02b1204a069f6099351d5ef3
xufangda <xfangda@gmail.com>
2019-08-19 22:06:25 +0100

    test pages

---
+++ page-36.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+        <div class="intro">
+            <div class="avatar">
+                <a href="/archives"><img src="<?php bloginfo('template_url');?>/image/header.jpg"></a>
+            </div>
+            <div class="nickname"><?php bloginfo('name'); ?></div>
+            <div class="description"><p><?php bloginfo('description'); ?></p></div>
+        
+            <div class="links">
+                <a class="link-item" title="Blog" href="/">
+                    <i class="iconfont icon-blog"></i>
+                </a>
+            
+                <a class="link-item" title="ZhiHu" href>
+                    <i class="iconfont icon-zhihu"></i>
+                </a>
+            
+                <a class="link-item" title="Instagram" href>
+                    <i class="iconfont icon-instagram"></i>
+                </a>
+        </div>
+    
+        
+<?php get_footer(); ?>
