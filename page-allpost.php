c51a56e518912bef6d14416dd99be91d051dcd73
xufangda <xfangda@gmail.com>
2019-08-20 21:45:14 +0100

    test

--- page-allpost.php
+++ page-allpost.php
@@ -15,14 +15,44 @@ Template Name: Blog List
     query_posts( 'posts_per_page=-1' );
     while ( have_posts() ) : the_post();
         echo '<article class="archive-item">';
-        echo '<a class="archive-item-link" href="the_permalink();">the_title();</a>';
-        echo '<span class="archive-item-date">the_time(get_option( 'date_format' ))</span>';
+        
+        // title
+        echo '<a class="archive-item-link" href="'; 
+        the_permalink();
+        echo '">';
+        the_title();
+        echo '</a>';
+        
+        // date 
+        echo '<span class="archive-item-date">'
+        the_time(get_option('date_format' ));
+        echo '</span>';
+        
+        echo '</article>';
         $published_posts--;
     endwhile;
     wp_reset_query(); ?>
  </div>
 
- 
+ <div id="page-allpost">
+     
+     <?php $count_posts = wp_count_posts(); $published_posts = $count_posts->publish;
+     query_posts( 'posts_per_page=-1' );
+     while ( have_posts() ) : the_post();
+         echo '<tr>';
+         echo '<td>'.$published_posts.'</td>';
+         echo '<td width="120">';
+         the_time(get_option( 'date_format' ));
+         echo '</td><td><a href="';
+         the_permalink();
+         echo '" title="'.esc_attr( get_the_title() ).'">';
+         the_title();
+         echo '</a></td></tr>';
+         $published_posts--;
+     endwhile;
+     wp_reset_query(); ?>
+     </table>
+ </div>
 
 <?php get_footer(); ?>
 
