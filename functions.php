0b0dfd6d758cee38f326e99d186f3efacdcd0521
xufangda <xfangda@gmail.com>
2019-08-20 23:34:04 +0100

    add header image

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 $args = array(
 	'width'         => 400,
 	'height'        => 400,
-	'default-image' => get_template_directory_uri() . '/images/header.jpg',
+	'default-image' => get_template_directory_uri() . '/image/header.jpg',
 );
 add_theme_support( 'custom-header', $args );
 
