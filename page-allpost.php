714f2334574dc4bd1cc0930c51e315b503c746d3
xufangda <xfangda@gmail.com>
2019-08-20 21:46:17 +0100

    test

--- page-allpost.php
+++ page-allpost.php
@@ -24,34 +24,14 @@ Template Name: Blog List
         echo '</a>';
         
         // date 
-        echo '<span class="archive-item-date">'
-        the_time(get_option('date_format' ));
-        echo '</span>';
+        // echo '<span class="archive-item-date">'
+        // the_time(get_option('date_format' ));
+        // echo '</span>';
         
         echo '</article>';
         $published_posts--;
     endwhile;
     wp_reset_query(); ?>
- </div>
-
- <div id="page-allpost">
-     
-     <?php $count_posts = wp_count_posts(); $published_posts = $count_posts->publish;
-     query_posts( 'posts_per_page=-1' );
-     while ( have_posts() ) : the_post();
-         echo '<tr>';
-         echo '<td>'.$published_posts.'</td>';
-         echo '<td width="120">';
-         the_time(get_option( 'date_format' ));
-         echo '</td><td><a href="';
-         the_permalink();
-         echo '" title="'.esc_attr( get_the_title() ).'">';
-         the_title();
-         echo '</a></td></tr>';
-         $published_posts--;
-     endwhile;
-     wp_reset_query(); ?>
-     </table>
  </div>
 
 <?php get_footer(); ?>
