36b7ae1496921daee2eaa2c6aa72ad147f963ec4
xufangda <xfangda@gmail.com>
2019-08-22 00:02:14 +0100

    comments

--- comments.php
+++ comments.php
@@ -28,67 +28,58 @@ if ( post_password_required() ) {
         die ('Please do not load this page directly. Thanks!');
 ?>
 
-<div id="comments" class="comments-area">
+	<div id="comments" class="comments-area">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$comments_number = get_comments_number();
-			if ( '1' === $comments_number ) {
-				/* translators: %s: post title */
-				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'twentyseventeen' ), get_the_title() );
-			} else {
-				printf(
-					/* translators: 1: number of comments, 2: post title */
-					_nx(
-						'%1$s Reply to &ldquo;%2$s&rdquo;',
-						'%1$s Replies to &ldquo;%2$s&rdquo;',
-						$comments_number,
-						'comments title',
-						'twentyseventeen'
-					),
-					number_format_i18n( $comments_number ),
-					get_the_title()
-				);
-			}
-			?>
-		</h2>
+	<?php 
+    if (!empty($post->post_password) && $_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) { 
+        // if there's a password
+        // and it doesn't match the cookie
+    ?>
+    <li class="decmt-box">
+        <p><a href="#addcomment">请输入密码再查看评论内容.</a></p>
+    </li>
+    <?php 
+        } else if ( !comments_open() ) {
+    ?>
+    <li class="decmt-box">
+        <p><a href="#addcomment">评论功能已经关闭!</a></p>
+    </li>
+    <?php 
+        } else if ( !have_comments() ) { 
+    ?>
+    <li class="decmt-box">
+        <p><a href="#addcomment">还没有任何评论，你来说两句吧</a></p>
+    </li>
+    <?php 
+        } else {
+            wp_list_comments('type=comment&callback=aurelius_comment');
+        }
+    ?>
 
-		<ol class="comment-list">
-			<?php
-				wp_list_comments(
-					array(
-						'avatar_size' => 100,
-						'style'       => 'ol',
-						'short_ping'  => true,
-						'reply_text'  => twentyseventeen_get_svg( array( 'icon' => 'mail-reply' ) ) . __( 'Reply', 'twentyseventeen' ),
-					)
-				);
-			?>
-		</ol>
-
-		<?php
-		the_comments_pagination(
-			array(
-				'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous', 'twentyseventeen' ) . '</span>',
-				'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-			)
-		);
-
-	endif; // Check for have_comments().
-
-	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-		?>
-
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentyseventeen' ); ?></p>
-		<?php
-	endif;
-
-	comment_form();
-	?>
-
+	<?php 
+    if (!empty($post->post_password) && $_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) { 
+        // if there's a password
+        // and it doesn't match the cookie
+    ?>
+    <li class="decmt-box">
+        <p><a href="#addcomment">请输入密码再查看评论内容.</a></p>
+    </li>
+    <?php 
+        } else if ( !comments_open() ) {
+    ?>
+    <li class="decmt-box">
+        <p><a href="#addcomment">评论功能已经关闭!</a></p>
+    </li>
+    <?php 
+        } else if ( !have_comments() ) { 
+    ?>
+    <li class="decmt-box">
+        <p><a href="#addcomment">还没有任何评论，你来说两句吧</a></p>
+    </li>
+    <?php 
+        } else {
+            wp_list_comments('type=comment&callback=aurelius_comment');
+        }
+    ?>
+	</div>
 </div><!-- #comments -->
