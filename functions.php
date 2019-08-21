87200174c97fd343aa4388ea7b03e18f19280869
xufangda <xfangda@gmail.com>
2019-08-21 13:48:46 +0100

    test

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $args = array(
 	'width'         => 400,
 	'height'        => 400,
@@ -8,16 +9,14 @@ $args = array(
 add_theme_support( 'custom-header', $args );
 
 
-function posts_link_attributes_prev() {
-    return 'class="prev"';
-}
-function posts_link_attributes_next() {
-    return 'class="next"';
-}
-
 add_filter('next_posts_link_attributes', 'posts_link_attributes_next');
 add_filter('previous_posts_link_attributes', 'posts_link_attributes_prev');
 
-
+function posts_link_attributes_prev() {
+    return 'class="prev"';
+};
+function posts_link_attributes_next() {
+    return 'class="next"';
+};
 
 ?>
