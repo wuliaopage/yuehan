b1f744be04446e8958d66d39b83b86a84aa7ef56
xufangda <xfangda@gmail.com>
2019-08-22 00:05:14 +0100

    comments

--- comments.php
+++ comments.php
@@ -52,34 +52,10 @@ if ( post_password_required() ) {
     </li>
     <?php 
         } else {
-            wp_list_comments('type=comment&callback=aurelius_comment');
+            wp_list_comments('type=comment');
         }
     ?>
 
-	<?php 
-    if (!empty($post->post_password) && $_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) { 
-        // if there's a password
-        // and it doesn't match the cookie
-    ?>
-    <li class="decmt-box">
-        <p><a href="#addcomment">请输入密码再查看评论内容.</a></p>
-    </li>
-    <?php 
-        } else if ( !comments_open() ) {
-    ?>
-    <li class="decmt-box">
-        <p><a href="#addcomment">评论功能已经关闭!</a></p>
-    </li>
-    <?php 
-        } else if ( !have_comments() ) { 
-    ?>
-    <li class="decmt-box">
-        <p><a href="#addcomment">还没有任何评论，你来说两句吧</a></p>
-    </li>
-    <?php 
-        } else {
-            wp_list_comments('type=comment&callback=aurelius_comment');
-        }
-    ?>
+	
 	</div>
 </div><!-- #comments -->
