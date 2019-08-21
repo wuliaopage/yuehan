67da374e3143ae7da102bb3489789bc50343a72b
xufangda <xfangda@gmail.com>
2019-08-21 14:02:27 +0100

    test

--- functions.php
+++ functions.php
@@ -9,14 +9,20 @@ $args = array(
 add_theme_support( 'custom-header', $args );
 
 
-add_filter('next_posts_link_attributes', 'posts_link_attributes_next');
+function filter_next_post_link($link) {
+    $link = str_replace("rel=", 'class="next" rel=', $link);
+    return $link;
+}
+add_filter('next_post_link', 'filter_next_post_link');
+
+// add_filter('next_posts_link_attributes', 'posts_link_attributes_next');
 // add_filter('previous_posts_link_attributes', 'posts_link_attributes_prev');
 
 // function posts_link_attributes_prev() {
 //     return 'class="prev"';
 // };
-function posts_link_attributes_next() {
-    return 'class="next"';
-};
+// function posts_link_attributes_next() {
+//     return 'class="next"';
+// };
 
 ?>
