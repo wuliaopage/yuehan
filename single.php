d360c78f35c32babed517bbf33b46bffb9e7df8c
xufangda <xfangda@gmail.com>
2019-08-21 02:25:59 +0100

    bug fix

--- single.php
+++ single.php
@@ -55,7 +55,27 @@
         
         <section class="post-tags">
             <div>
-                <span>Tag(s):</span>
+            <?php if ( get_edit_post_link() ) : ?>
+            <?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'freeware' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				),
+				'<span>',
+				'</span>'
+			);
+			?>
+                
+                <!-- <span>Tag(s):</span> -->
                 <span class="tag">
                     
                 </span>
