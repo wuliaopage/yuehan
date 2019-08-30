33b6b142c0ae11b1dbbb7a97b96b1c4de4f0ad40
xufangda <xfangda@gmail.com>
2019-08-30 10:43:38 +0100

    page logic to be added

--- comments.php
+++ comments.php
@@ -72,13 +72,13 @@ if ( post_password_required() ) {
 			?>
 		</ol><!-- .commentlist -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="navigation" role="navigation">
+		<?php //if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<!-- <nav id="comment-nav-below" class="navigation" role="navigation">
 			<h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'twentytwelve' ); ?></h1>
 			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentytwelve' ) ); ?></div>
 			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentytwelve' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
+		</nav> -->
+		<?php //endif; // check for comment navigation ?>
 
 		<?php
 		/* If there are no comments and comments are closed, let's leave a note.
