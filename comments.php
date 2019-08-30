f353817f335493d4992c1fd0068b5971dc4c4ec0
xufangda <xfangda@gmail.com>
2019-08-30 15:59:30 +0100

    comment

--- comments.php
+++ comments.php
@@ -41,17 +41,17 @@ if ( post_password_required() ) {
 
 	<?php // You can start editing here -- including this comment! ?>
 	<?php 
-		$comments_args = array(
-        // Change the title of send button 
-        'label_submit' => __( 'Send', 'textdomain' ),
-        // Change the title of the reply section
-        'title_reply' => __( 'Write a Reply or Comment', 'textdomain' ),
-        // Remove "Text or HTML to be displayed after the set of comment fields".
-        'comment_notes_after' => '',
-        // Redefine your own textarea (the comment body).
-        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
-		);
-		comment_form( $comments_args );
+		// $comments_args = array(
+        // // Change the title of send button 
+        // 'label_submit' => __( 'Send', 'textdomain' ),
+        // // Change the title of the reply section
+        // 'title_reply' => __( 'Write a Reply or Comment', 'textdomain' ),
+        // // Remove "Text or HTML to be displayed after the set of comment fields".
+        // 'comment_notes_after' => '',
+        // // Redefine your own textarea (the comment body).
+        // 'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+		// );
+		comment_form();
 	 ?>
 
 	<?php if ( have_comments() ) : ?>
