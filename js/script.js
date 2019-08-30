31911dd0c46b2b7339e420310039b428976875f8
xufangda <xfangda@gmail.com>
2019-08-30 22:11:14 +0100

    comment

--- js/script.js
+++ js/script.js
@@ -45,23 +45,7 @@ document.ready(
             // mobile
             document.getElementById("mobile-toggle-theme").innerText = "· Dark"
         }
-        const currentComment = window.localStorage && window.localStorage.getItem('comment');
-        const isHide = currentComment === 'hide';
-        var toggleMenu = document.getElementsByClassName("comments-area")[0];
-        console.log('fuck')
-        if (isHide) {
-            document.getElementById("comment_default").checked = true;
-            toggleMenu.classList.remove("comment-hide");  
-            console.log('YH')
         
-        } else {
-            document.getElementById("comment_default").checked = false;
-            toggleMenu.classList.add("comment-hide");
-            console.log('L')
-        
-        }
-
-
         _Blog.toggleTheme = function () {
             if (isDark) {
                 document.getElementsByTagName('body')[0].classList.add('dark-theme');
