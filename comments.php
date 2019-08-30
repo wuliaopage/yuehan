4f81c961ec6f8b6c19252b20f34d0e7331234147
xufangda <xfangda@gmail.com>
2019-08-30 15:57:35 +0100

    comment

--- comments.php
+++ comments.php
@@ -40,6 +40,19 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 
 	<?php // You can start editing here -- including this comment! ?>
+	<?php 
+		$comments_args = array(
+        // Change the title of send button 
+        'label_submit' => __( 'Send', 'textdomain' ),
+        // Change the title of the reply section
+        'title_reply' => __( 'Write a Reply or Comment', 'textdomain' ),
+        // Remove "Text or HTML to be displayed after the set of comment fields".
+        'comment_notes_after' => '',
+        // Redefine your own textarea (the comment body).
+        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+		);
+		comment_form( $comments_args );
+	 ?>
 
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
@@ -91,7 +104,7 @@ if ( post_password_required() ) {
 
 	<?php endif; // have_comments() ?>
 
-	<?php comment_form(); ?>
+	
 
 </div><!-- #comments .comments-area -->
 
