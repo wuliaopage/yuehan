b0e342519fe9d0abefe14bfe9f2e99c98b1a2c53
xufangda <xfangda@gmail.com>
2019-08-30 16:53:23 +0100

    test

--- comments.php
+++ comments.php
@@ -77,7 +77,7 @@ if ( post_password_required() ) {
 			'comment_notes_after' => '',
 			'comment_notes_before' => '',
 			// Redefine your own textarea (the comment body).
-			'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>'
+			'comment_field' => '<p class="comment-form-comment"><textarea id="comment" name="comment" aria-required="true"></textarea></p>'
 
 		);
 		comment_form( $args ); 
