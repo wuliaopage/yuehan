dcedb06778deef4d40d147c1d98326d24e4a32a4
xufangda <xfangda@gmail.com>
2019-09-03 17:10:25 +0100

    google analytics added

--- header.php
+++ header.php
@@ -6,6 +6,16 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-71315338-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-71315338-2');
+</script>
+
 <!-- Title for SEO -->
 <title>
 <?php if ( is_home() ) {
